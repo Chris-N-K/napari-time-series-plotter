9a9d7a5e8f7b1b70c6dae14250304a31db93e286
cnk <nauroth_c@ukw.de>
2021-11-16 14:35:18 +0100

    Added in code documentation for the LayerSelector class

--- src/napari_timeseries_viewer/_dock_widget.py
+++ src/napari_timeseries_viewer/_dock_widget.py
@@ -18,45 +18,80 @@ from qtpy.QtWidgets import QWidget, QCheckBox, QVBoxLayout
 
 
 class LayerSelector(QWidget):
+    """A class to generate a widget for layer selection.
+
+    This class generates a pyqt.QWidget containing pyqt.QCheckBoxes for each layer present in the current napari viewer
+    instance. The widget is connected to napari viewer events, thus pyqt.QCheckBoxes are removed or added to the widget
+    if layers are added or removed. The class stores references to layers of checked boxes in self.selected_layers for
+    access from the outside.
+
+    :param napari_viewer: Napari viewer instance, input should be handeled by the napari_hook_implementation decoration
+    :type napari_viewer: napari.Viewer() object
+    """
     def __init__(self, napari_viewer):
         super().__init__()
 
         self.viewer = napari_viewer
         self.selected_layers = []
-        self.cboxes = []
-        self.init_ui()
+        self._cboxes = []
+        self._init_ui()
 
-    def add_cb(self, *args):
-        w_names = [cb.text() for cb in self.cboxes]
+    def _add_cb(self, *args):
+        """Function to add a pyqt.QCheckBox to the LayerSelector object.
+
+        The function scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in
+        the napari viewer instance. For layers not corresponding to any existing pyqt.QCheckboxes new widgets are added.
+        The function accepts additional arguments to offer compatability with napari viewer event hooks.
+        State change events of the checkboxes are connected to the internal _check_states() function.
+        """
+        w_names = [cb.text() for cb in self._cboxes]
         for layer in self.viewer.layers:
             if layer.name not in w_names:
                 cb = QCheckBox(layer.name, self)
-                cb.stateChanged.connect(self.check_states)
+                cb.stateChanged.connect(self._check_states)
                 self.vbox.addWidget(cb)
-                self.cboxes.append(cb)
+                self._cboxes.append(cb)
 
-    def remove_cb(self, *args):
-        for cb in self.cboxes:
+    def _remove_cb(self, *args):
+        """Function to add a pyqt.QCheckBox to the LayerSelector object.
+
+        The function scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in
+        the napari viewer instance. Any pyqt.QCheckboxes not corresponding to viewer layers are removed.
+        The function accepts additional arguments to offer compatability with napari viewer event hooks.
+        """
+        for cb in self._cboxes:
             if cb.text() not in self.viewer.layers:
                 cb.deleteLater()
-                self.cboxes.remove(cb)
+                self._cboxes.remove(cb)
 
-    def init_ui(self):
+    def _init_ui(self):
+        """Function to initialise the LayerSelector widget UI.
+
+        This function generates a pyqt.QVBoxLayout and executes the add_cb() function to populate it with
+        pyqt.QCheckboxes. The internal function _add_cb() is connected to the napari.Viewer.layers.events.inserted event
+        and _remove_cb() to napari.Viewer.layers.event.removed event.
+        """
         # creat outer surroundings
         self.vbox = QVBoxLayout()
 
         # create check boxes
         self.setLayout(self.vbox)
         self.setWindowTitle('Layer Selector')
-        self.add_cb()
+        self._add_cb()
 
         # connect checkbox management with viewer layer events
-        self.viewer.layers.events.removed.connect(self.remove_cb)
-        self.viewer.layers.events.inserted.connect(self.add_cb)
+        self.viewer.layers.events.removed.connect(self._remove_cb)
+        self.viewer.layers.events.inserted.connect(self._add_cb)
 
-    def check_states(self):
+    def _check_states(self):
+        """Function to check pyqt.QCheckbox states.
+
+        This function curates the attribute selected_layers of LayerSelector objects. If called all pyqt.QCheckboxes
+        registered in the internal _cboxes list are checked for there current state. If a box's state is checked the
+        corresponding napari viewer layers is added to the list of selected layers.
+        """
         self.selected_layers = []
-        for cb in self.cboxes:
+        for cb in self._cboxes:
             if cb.isChecked():
                 for layer in self.viewer.layers:
                     if layer.name == cb.text():
