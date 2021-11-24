04bc3d6990051c382be8379fb8d3ab46fb972c4c
cnk <nauroth_c@ukw.de>
2021-11-24 12:55:39 +0100

    introduced better widget to widget communication and complete rework of the VoxelPlotter class

--- src/napari_timeseries_viewer/_dock_widget.py
+++ src/napari_timeseries_viewer/_dock_widget.py
@@ -7,14 +7,46 @@ see: https://napari.org/docs/dev/plugins/hook_specifications.html
 
 Replace code below according to your needs.
 """
-import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvas
+# from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+
+from napari.layers.image import Image
+from napari_plugin_engine import napari_hook_implementation
 
 import numpy as np
-import warnings
 
-from napari_plugin_engine import napari_hook_implementation
-from qtpy.QtWidgets import QWidget, QCheckBox, QVBoxLayout
+from qtpy.QtCore import Signal, Slot, QObject
+from qtpy.QtWidgets import QDialog, QWidget, QCheckBox, QVBoxLayout
+
+matplotlib.use('QT5Agg')
+
+# Thanks a lot to Grzegorz Bokota for pointing out this solution for connecting the widgets!
+_sync = None
+
+
+class Sync(QObject):
+    signal = Signal([list])
+
+
+def get_sync():
+    global _sync
+    if _sync is None:
+        _sync = Sync()
+    return _sync
+##############################################################################################
+
+
+def extract_voxel_time_series(cpos, nlayer):
+    # get full data array from layer
+    data = nlayer.data
+    # convert cursor position to index
+    ind = tuple(map(int, nlayer.world_to_data(cpos)[1:]))
+    # return extracted data if index matches array
+    if all([0 <= i < max_i for i, max_i in zip(ind, data.shape)]):
+        return data[(slice(None),) + ind]
+    return None
 
 
 class LayerSelector(QWidget):
@@ -30,46 +62,22 @@ class LayerSelector(QWidget):
     """
     def __init__(self, napari_viewer):
         super().__init__()
-
         self.viewer = napari_viewer
         self.selected_layers = []
         self._cboxes = []
+        self.sync = get_sync()
         self._init_ui()
 
-    def _add_cb(self, *args):
-        """Function to add a pyqt.QCheckBox to the LayerSelector object.
-
-        The function scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in
-        the napari viewer instance. For layers not corresponding to any existing pyqt.QCheckboxes new widgets are added.
-        The function accepts additional arguments to offer compatability with napari viewer event hooks.
-        State change events of the checkboxes are connected to the internal _check_states() function.
-        """
-        w_names = [cb.text() for cb in self._cboxes]
-        for layer in self.viewer.layers:
-            if layer.name not in w_names:
-                cb = QCheckBox(layer.name, self)
-                cb.stateChanged.connect(self._check_states)
-                self.vbox.addWidget(cb)
-                self._cboxes.append(cb)
-
-    def _remove_cb(self, *args):
-        """Function to add a pyqt.QCheckBox to the LayerSelector object.
-
-        The function scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in
-        the napari viewer instance. Any pyqt.QCheckboxes not corresponding to viewer layers are removed.
-        The function accepts additional arguments to offer compatability with napari viewer event hooks.
-        """
-        for cb in self._cboxes:
-            if cb.text() not in self.viewer.layers:
-                cb.deleteLater()
-                self._cboxes.remove(cb)
+        # connect checkbox management with viewer layer events
+        self.viewer.layers.events.removed.connect(self._remove_cb)
+        self.viewer.layers.events.inserted.connect(self._add_cb)
 
     def _init_ui(self):
-        """Function to initialise the LayerSelector widget UI.
+        """Method to initialise the LayerSelector widget UI.
 
-        This function generates a pyqt.QVBoxLayout and executes the add_cb() function to populate it with
-        pyqt.QCheckboxes. The internal function _add_cb() is connected to the napari.Viewer.layers.events.inserted event
-        and _remove_cb() to napari.Viewer.layers.event.removed event.
+        This method generates a pyqt.QVBoxLayout and executes the _add_cb() method to populate it with pyqt.QCheckboxes.
+        _add_cb() is connected to the napari.Viewer.layers.events.inserted event and _remove_cb() to
+        napari.Viewer.layers.event.removed event.
         """
         # creat outer surroundings
         self.vbox = QVBoxLayout()
@@ -79,14 +87,39 @@ class LayerSelector(QWidget):
         self.setWindowTitle('Layer Selector')
         self._add_cb()
 
-        # connect checkbox management with viewer layer events
-        self.viewer.layers.events.removed.connect(self._remove_cb)
-        self.viewer.layers.events.inserted.connect(self._add_cb)
+    def _add_cb(self, *args):
+        """Method to add a pyqt.QCheckBox to the LayerSelector object.
+
+        The method scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in the napari
+        viewer instance. For layers not corresponding to any existing pyqt.QCheckboxes new widgets are added.
+        The method accepts additional arguments to be compatible with napari viewer event hooks.
+        State change events of the checkboxes are connected to the internal _check_states() function.
+        """
+        w_names = [cb.text() for cb in self._cboxes]
+        for layer in self.viewer.layers:
+            if layer.name not in w_names and isinstance(layer, Image):
+                if 5 > layer.ndim > 2:
+                    cb = QCheckBox(layer.name, self)
+                    cb.stateChanged.connect(self._check_states)
+                    self.vbox.addWidget(cb)
+                    self._cboxes.append(cb)
+
+    def _remove_cb(self, *args):
+        """Method to add a pyqt.QCheckBox to the LayerSelector object.
+
+        The method scans the LayerSelector object for active pyqt.QCheckboxes and the list of layers in
+        the napari viewer instance. Any pyqt.QCheckboxes not corresponding to viewer layers are removed.
+        The method accepts additional arguments to be compatible with napari viewer event hooks.
+        """
+        for cb in self._cboxes:
+            if cb.text() not in self.viewer.layers:
+                cb.deleteLater()
+                self._cboxes.remove(cb)
 
     def _check_states(self):
-        """Function to check pyqt.QCheckbox states.
+        """Method to check pyqt.QCheckbox states.
 
-        This function curates the attribute selected_layers of LayerSelector objects. If called all pyqt.QCheckboxes
+        This method curates the attribute selected_layers of LayerSelector objects. If called all pyqt.QCheckboxes
         registered in the internal _cboxes list are checked for there current state. If a box's state is checked the
         corresponding napari viewer layers is added to the list of selected layers.
         """
@@ -96,21 +129,33 @@ class LayerSelector(QWidget):
                 for layer in self.viewer.layers:
                     if layer.name == cb.text():
                         self.selected_layers.append(layer)
+        self.sync.signal.emit(self.selected_layers)
 
 
 # TODO: Add in code documentation for VoxelPlotter class and methods
-class VoxelPlotter:
+class VoxelPlotter(QDialog):
+    """
 
-    def __init__(self, napari_viewer,):
-        super().__init__()
+    """
+    def __init__(self, napari_viewer, parent=None):
+        super(VoxelPlotter, self).__init__(parent)
         self.viewer = napari_viewer
-        self._initMPL()
+        self.selected_layers = []
+        self.sync = get_sync()
+        self._initMPLwidgets()
 
-    def _initMPL(self):
+        # connect callbacks
+        self.sync.signal.connect(self._update_layer_selection)
+        self.viewer.mouse_move_callbacks.append(self._plot_voxel_callback)
+
+    def _initMPLwidgets(self):
         # set up figure and axe objects
-        self.fig = plt.figure(figsize=(5, 5))
+        self.fig = Figure()
+        self.canvas = FigureCanvas(self.fig)
+        # TODO: find a good way to include the toolbar with a compatible style
+        # self.toolbar = NavigationToolbar(self.canvas, self)
+        # self.toolbar.setStyleSheet("color:Black;")
         self.ax = self.fig.add_subplot(111)
-        self.fig.patch.set_facecolor('white')
         self.ax.annotate('Hold "Shift" while moving over the image'
                          '\nto plot pixel signal over time',
                          (0.5, 0.5),
@@ -118,51 +163,47 @@ class VoxelPlotter:
                          va='center',
                          size=15,
                          bbox=dict(facecolor=(0.9, 0.9, 0.9), alpha=1, boxstyle='square'))
-        mpl_widget = FigureCanvas(self.fig)
-        self.viewer.window.add_dock_widget(mpl_widget, name='Voxel Plotter', area='right')
 
-        # callback function for voxel readout
-        @self.viewer.mouse_move_callbacks.append
-        def plot_voxel_callback(viewer, event):
-            """Mouse movement callback function.
+        # construct layout
+        layout = QVBoxLayout()
+        # layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        self.setLayout(layout)
+        self.setWindowTitle('Voxel Plotter')
 
-            """
+    @Slot(list)
+    def _update_layer_selection(self, selected_layers):
+        """Method for updating the selected_layers attribute.
 
-            if 'Shift' in event.modifiers:
-                # get cursor position from viewer
-                cursor_pos = np.round(viewer.cursor.position)
+        :param selected_layers: list of napari image layers
+        """
+        self.selected_layers = selected_layers
 
-                # get selected layers form layer selector
-                selected_layers = self.viewer.window._dock_widgets['Layer Selector'].widget().selected_layers
+    def _plot_voxel_callback(self, viewer, event):
+        """Mouse movement callback function.
 
-                # clear active plot
-                self.ax.cla()
+        """
+        if 'Shift' in event.modifiers:
+            # get cursor position from viewer
+            cursor_pos = np.round(viewer.cursor.position)
+            # clear active plot
+            self.ax.clear()
+            # add new graphs
+            for layer in self.selected_layers:
+                # get layer data
+                lname = layer.name
 
-                # add new graphs
-                for layer in selected_layers:
-                    # get layer data
-                    lname = layer.name
-                    ndim = layer.ndim
-                    data = layer.data
+                # extract voxel time series
+                vts = extract_voxel_time_series(cursor_pos, layer)
+                if not isinstance(vts, type(None)):
+                    # add graph
+                    self.ax.plot(vts, label=lname)
+                    self.ax.set_xlabel('Time')
+                    self.ax.set_ylabel('Pixel / Voxel Value')
 
-                    if 3 > ndim or ndim > 4:
-                        warnings.warn(f'Only layers with three or four dimensions are supported.'
-                                      f'\nSelected layer: {lname}, has {ndim} dimensions.')
-                    else:
-                        # extract voxel time series
-                        ind = tuple(map(int, layer.world_to_data(cursor_pos)[1:]))
-                        vdata = data[(slice(None),) + ind]
-
-                        # add graph
-                        self.ax.plot(vdata, label=lname)
-                        self.ax.set_xlabel('Time')
-                        self.ax.set_ylabel('Pixel / Voxel Value')
-
-                # redraw figure
-                self.ax.legend(loc=1)
-                self.fig.canvas.draw()
-                plt.tight_layout()
-            plt.close()
+            # redraw figure
+            self.ax.legend(loc=1)
+            self.fig.canvas.draw()
 
 
 @napari_hook_implementation
--- src/napari_timeseries_viewer/_tests/test_dock_widget.py
+++ src/napari_timeseries_viewer/_tests/test_dock_widget.py
@@ -1,4 +1,4 @@
-from napari_timeseries_viewer._dock_widget import LayerSelector, VoxelPlotter
+from napari_timeseries_viewer._dock_widget import *
 import pytest
 import numpy as np
 
@@ -6,8 +6,8 @@ import numpy as np
 # fixture for LayerSelector class tests
 @pytest.fixture
 def selector(make_napari_viewer):
+    _sync = None
     viewer = make_napari_viewer(show=False)
-    viewer.add_image(np.random.rand(10, 10), name='2D')
     viewer.add_image(np.random.rand(10, 10, 10), name='3D')
     yield LayerSelector(viewer)
 
@@ -15,58 +15,116 @@ def selector(make_napari_viewer):
 # fixture for VoxelPlotter class tests
 @pytest.fixture
 def plotter(make_napari_viewer):
+    _sync = None
     viewer = make_napari_viewer(show=False)
-    viewer.add_image(np.random.rand(10, 10), name='2D')
     viewer.add_image(np.random.rand(10, 10, 10), name='3D')
     viewer.add_image(np.random.rand(10, 10, 10, 10), name='4D')
-    viewer.add_image(np.random.rand(10, 10, 10, 10, 10), name='5D')
-    layer_selector = LayerSelector(viewer)
-    viewer.window.add_dock_widget(layer_selector, name='Layer Selector', area='right')
     yield VoxelPlotter(viewer)
 
 
-# test LayerSelector
-# the widget should already contain four checkboxes named after the layers of viewer
+# test fixtures
 def test_selector(selector: LayerSelector):
     assert selector.windowTitle() == 'Layer Selector'
+    # the widget should already contain four checkboxes named after the layers of viewer
     layers = [layer.name for layer in selector.viewer.layers]
     cboxes = [cb.text() for cb in selector._cboxes]
     assert all([lname == cbname for lname, cbname in zip(layers, cboxes)])
 
+    # Sync instance generated?
+    assert isinstance(selector.sync, Sync)
 
-# when a layer is added to the viewer a new checkbox should appear
+
+def test_plotter(plotter: VoxelPlotter):
+    # spawned mpl_widget and added to viewer?
+    assert plotter.windowTitle() == 'Voxel Plotter'
+
+    # Sync instance generated?
+    assert isinstance(plotter.sync, Sync)
+
+
+# test LayerSelector subunits
 def test_selector_add_layer(selector: LayerSelector):
-    assert len(selector._cboxes) == 2
-    selector.viewer.add_image(np.random.rand(10, 10))
-    assert len(selector._cboxes) == 3
-
-
-# when a layer is removed from the viewer the corresponding checkbox should be removed
-def test_selector_remove_layer(selector: LayerSelector):
-    assert len(selector._cboxes) == 2
-    selector.viewer.layers.remove('2D')
+    # should start with one checkbox
     assert len(selector._cboxes) == 1
 
+    # adding a 4D image layer should trigger checkbox generation
+    selector.viewer.add_image(np.random.rand(10, 10, 10, 10), name='4D')
+    assert len(selector._cboxes) == 2
 
-# test if the state of the checkboxes is correctly translated
-def test_selector_select_layer(selector: LayerSelector):
+    # 2D and 5D images layers as well as other layer types should not trigger checkbox generation
+    selector.viewer.add_image(np.random.rand(10, 10), name='2D')
+    selector.viewer.add_image(np.random.rand(10, 10, 10, 10, 10), name='5D')
+    selector.viewer.add_labels(np.random.randint(10, size=(10, 10, 10)), name='3D_labels')
+    assert len(selector._cboxes) == 2
+
+
+def test_selector_remove_layer(selector: LayerSelector):
+    # should start with one checkbox
+    assert len(selector._cboxes) == 1
+
+    # removing a layer should delete the corresponding checkbox
+    selector.viewer.layers.remove('3D')
+    assert len(selector._cboxes) == 0
+
+
+def test_selector_selection_response(selector: LayerSelector):
     # initially no boxes should be checked and selected_layers should be empty
     assert not all([cb.isChecked() for cb in selector._cboxes])
     assert not selector.selected_layers
 
-    # after checking the boxes selected_layers should contain all viewer layers corresponding to the checked boxes
-    for cb in selector._cboxes[:1]:
-        cb.setChecked(True)
-    # two layers selected?
-    assert len(selector.selected_layers) == 1
-    # where the right layers selected?
-    assert all([slayer == vlayer for slayer, vlayer in zip(selector.selected_layers, selector.viewer.layers[:1])])
+    # if a box is checked a signal should be emitted
+    def _receiver_mock(signal):
+        assert signal == selector.selected_layers
+        assert '3D' == signal[0].name
+
+    selector.sync.signal.connect(_receiver_mock)
+    for cb in selector._cboxes:
+        if cb.text() == '3D':
+            cb.setChecked(True)
 
 
-# TODO: Add VoxelPlotter tests
-# test VoxelPlotter
-def test_plotter(plotter: VoxelPlotter):
-    # spawned mpl_widget and added to viewer?
-    assert 'Voxel Plotter' in plotter.viewer.window._dock_widgets
-    assert plotter.fig
-    assert plotter.ax
+# test VoxelPlotter subunits
+def test_plotter_layer_selection_update(plotter: VoxelPlotter):
+    # at start no layers should be selected
+    assert not plotter.selected_layers
+
+    # TODO: find more optimal solution for the signal test
+    # if a signal was detected selected_layers should be updated
+    layers = plotter.viewer.layers
+    mock_selected_layers = [layers['3D'], layers['4D']]
+    plotter._update_layer_selection(mock_selected_layers)
+    assert plotter.selected_layers == mock_selected_layers
+
+
+# TODO: write test for the plot voxel callback
+def test_plotter_plot_voxel_callback(plotter: VoxelPlotter):
+    pass
+
+
+# test functions
+def test_extract_voxel_time_series(make_napari_viewer, monkeypatch):
+    viewer = make_napari_viewer(show=False)
+    viewer.add_image(np.random.rand(10, 10, 10, 10), name='4D')
+
+    # mock cursor position
+    cursor_pos_1 = [3, 3, 3, 3]
+    cursor_pos_2 = [3, -3, 3, 3]
+
+    # select layers
+    layer = viewer.layers['4D']
+
+    # mock data at [:, 3, 3, 3]
+    mock_data = [i for i in range(layer.data.shape[0])]
+    layer.data[:, 3, 3, 3] = mock_data
+
+    # extracted data should be equal mock data
+    vts = extract_voxel_time_series(cursor_pos_1, layer)
+    assert np.all(vts == mock_data)
+
+    # cursor position outside of the array index should yield no data
+    vts = extract_voxel_time_series(cursor_pos_2, layer)
+    assert not vts
+
+
+def test_napari_experimental_provide_dock_widget():
+    assert all([a == b for a, b in zip([LayerSelector, VoxelPlotter], napari_experimental_provide_dock_widget())])
